3b8bf034e4181c50fac66d0c6d603b68d13e3bc0
Ivan Abukhovskiy <i.abukhovskiy@carx-tech.com>
2025-03-27 11:37:37 +0300

    Initial commit + lesson 2.1

---
+++ xlab-abukhovskiy-unit2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xlab_abukhovskiy_unit2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b;
+            b = a;
+
+            byte b;
+            sbyte sb;
+            short s;
+            ushort us;
+            int i;
+            uint ui;
+            long l;
+            ulong ul;
+
+            float f = 6.2f;
+            double d = 5.7;
+
+            char c = 'f';
+
+            string str = "stro4ka";
+
+            bool bl;
+        }
+    }
+}
